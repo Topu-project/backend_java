3c067ce048b9cb92e7aa52bde1df3b1e4e7cbd4f
尹健雄 <yun@yun-mbp.local>
2024-08-22 09:02:04 +0900

    first init

---
+++ src/test/java/jp/co/topucomunity/backend_java/BackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.topucomunity.backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
