dc1bf3ec5bd2e2579af52e0d0a437353bcf85fdf
falsystack <falsystack@gmail.com>
2024-09-16 23:46:19 +0900

    TOPU-35 feat: user sign up

--- src/main/java/jp/co/topucomunity/backend_java/users/domain/User.java
+++ src/main/java/jp/co/topucomunity/backend_java/users/domain/User.java
@@ -2,10 +2,16 @@ package jp.co.topucomunity.backend_java.users.domain;
 
 import jakarta.persistence.*;
 import jp.co.topucomunity.backend_java.recruitments.domain.Position;
+import jp.co.topucomunity.backend_java.recruitments.domain.TechStack;
+import jp.co.topucomunity.backend_java.users.usecase.in.RegisterUser;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Table(name = "users")
@@ -29,25 +35,33 @@ public class User {
 
     private String affiliation;
 
-    private Boolean isPublicAffiliation;
+    private boolean isPublicAffiliation;
+
+    @ColumnDefault(value = "true")
+    private boolean isFirstLogin;
 
     private Integer personalHistoryYear;
 
     @Lob
     private String selfIntroduction;
 
+    @OneToMany(cascade = CascadeType.PERSIST)
+    private List<TechStack> techStacks = new ArrayList<>();
+
     private String links;
 
     @Builder
-    public User(String sub, String email, String nickname, Position position, String affiliation, Boolean isPublicAffiliation, Integer personalHistoryYear, String selfIntroduction, String links) {
+    public User(String sub, String email, String nickname, Position position, String affiliation, boolean isPublicAffiliation, boolean isFirstLogin, Integer personalHistoryYear, String selfIntroduction, List<TechStack> techStacks, String links) {
         this.sub = sub;
         this.email = email;
         this.nickname = nickname;
         this.position = position;
         this.affiliation = affiliation;
         this.isPublicAffiliation = isPublicAffiliation;
+        this.isFirstLogin = isFirstLogin;
         this.personalHistoryYear = personalHistoryYear;
         this.selfIntroduction = selfIntroduction;
+        this.techStacks = techStacks;
         this.links = links;
     }
 
@@ -55,19 +69,32 @@ public class User {
         return User.builder()
                 .sub(sub)
                 .email(email)
+                .isFirstLogin(true)
                 .build();
     }
 
-    public static User createFirstLoggedInUser(String email, String nickname, Position position, String affiliation, Boolean isPublicAffiliation, Integer personalHistoryYear, String selfIntroduction, String links) {
+    public static User createFirstLoggedInUser(String email, String nickname, Position position, String affiliation, boolean isPublicAffiliation, Integer personalHistoryYear, String selfIntroduction, String links, boolean isFirstLogin) {
         return User.builder()
                 .email(email)
                 .nickname(nickname)
                 .position(position)
                 .affiliation(affiliation)
                 .isPublicAffiliation(isPublicAffiliation)
+                .isFirstLogin(isFirstLogin)
                 .personalHistoryYear(personalHistoryYear)
                 .selfIntroduction(selfIntroduction)
                 .links(links)
                 .build();
+    }
+
+    public void registerFirstLoginUserInfo(RegisterUser registerUser) {
+        this.nickname = registerUser.getNickname();
+        this.position = Position.from(registerUser.getPositionName());
+        this.personalHistoryYear = registerUser.getPersonalHistoryYear();
+        this.isPublicAffiliation = false;
+        this.isFirstLogin = false;
+        this.techStacks = registerUser.getTechStackNames().stream()
+                .map(TechStack::from)
+                .toList();
     }
 }
--- src/main/java/jp/co/topucomunity/backend_java/users/usecase/UserUsecase.java
+++ src/main/java/jp/co/topucomunity/backend_java/users/usecase/UserUsecase.java
@@ -1,10 +1,13 @@
 package jp.co.topucomunity.backend_java.users.usecase;
 
 import jp.co.topucomunity.backend_java.users.controller.out.UserResponse;
+import jp.co.topucomunity.backend_java.users.exception.AlreadyExistUserException;
 import jp.co.topucomunity.backend_java.users.exception.UserNotFoundException;
 import jp.co.topucomunity.backend_java.users.repository.UserRepository;
+import jp.co.topucomunity.backend_java.users.usecase.in.RegisterUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +18,17 @@ public class UserUsecase {
     public UserResponse getUser(Long userId) {
         var foundUser = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
         return UserResponse.from(foundUser);
+    }
+
+    @Transactional
+    public void signUp(RegisterUser registerUser) {
+        var foundUser = userRepository.findById(registerUser.getUserId())
+                .orElseThrow(UserNotFoundException::new);
+
+        if (!foundUser.isFirstLogin()) {
+            throw new AlreadyExistUserException();
+        }
+
+        foundUser.registerFirstLoginUserInfo(registerUser);
     }
 }
