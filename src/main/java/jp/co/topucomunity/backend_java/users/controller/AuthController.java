e9ac1e3d1b72e6ae0bf98dcdfa7f0ce2268797bc
falsystack <falsystack@gmail.com>
2024-10-07 23:46:19 +0900

    TOPU-50 feat: decode token

--- src/main/java/jp/co/topucomunity/backend_java/users/controller/AuthController.java
+++ src/main/java/jp/co/topucomunity/backend_java/users/controller/AuthController.java
@@ -3,6 +3,8 @@ package jp.co.topucomunity.backend_java.users.controller;
 import jp.co.topucomunity.backend_java.users.controller.in.SignUpRequest;
 import jp.co.topucomunity.backend_java.users.controller.out.UserResponse;
 import jp.co.topucomunity.backend_java.users.domain.UserSession;
+import jp.co.topucomunity.backend_java.users.exception.UnAuthenticationException;
+import jp.co.topucomunity.backend_java.users.repository.UserRepository;
 import jp.co.topucomunity.backend_java.users.usecase.UserUsecase;
 import jp.co.topucomunity.backend_java.users.usecase.in.RegisterUser;
 import lombok.RequiredArgsConstructor;
@@ -10,10 +12,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.Map;
 
 @Slf4j
 @RestController
@@ -22,6 +31,9 @@ import java.net.URI;
 public class AuthController {
 
     private final UserUsecase userUsecase;
+    private final ClientRegistrationRepository clientRegistrationRepository;
+    private final UserRepository userRepository;
+
 
     @GetMapping("/login")
     public ResponseEntity<?> login(@Validated UserSession userSession) {
@@ -42,4 +54,36 @@ public class AuthController {
         userUsecase.signUp(RegisterUser.of(Long.valueOf(userSession.id()), request));
     }
 
+    @PostMapping("exchange-token")
+    public ResponseEntity<Map<String, Object>> exchangeCode(@RequestBody Map<String, String> request) {
+        String idTokenValue = request.get("idToken");
+        if (idTokenValue == null) {
+            throw new UnAuthenticationException();
+        }
+
+        // 구글 클라이언트 등록 정보 가져오기
+        ClientRegistration clientRegistration = clientRegistrationRepository.findByRegistrationId("google");
+
+        // TODO JWT Token 검증용 클래스 생성 - ID 토큰 검증
+        JwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(clientRegistration.getProviderDetails().getIssuerUri());
+        Jwt jwt = null;
+        try {
+            jwt = jwtDecoder.decode(idTokenValue);
+        } catch (JwtException e) {
+            System.out.println("e = " + e);
+        }
+
+        // 사용자 정보 추출
+        String sub = jwt.getSubject();
+        String email = jwt.getClaimAsString("email");
+        String name = jwt.getClaimAsString("name");
+        String picture = jwt.getClaimAsString("picture");
+
+
+        // TODO User DB에 등록
+
+        // TODO 사용자 ID 반환용 DTO 작성
+        return ResponseEntity.ok(Map.of("userId", "test"));
+
+    }
 }
